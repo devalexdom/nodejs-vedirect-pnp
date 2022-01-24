cb5a884bf0528e0847039670eec3711b33ae8ca6
Àlex Dom <devalexdom@gmail.com>
2022-01-24 22:06:25 +0100

    Code refactor & implement DeviceData interface

--- src/deviceData.ts
+++ src/deviceData.ts
@@ -1,87 +1,27 @@
 import deviceName from "./data/deviceName";
+import { StatusMessage, ErrorMessage, MPPTMessage, DeviceType, OffReasonMessage } from "./data/device-data-enum";
+import { VEDirectData } from "./ve-direct";
 
-enum StatusMessage {
-    "Off" = 0,
-    "Low power" = 1,
-    "Fault" = 2,
-    "Bulk" = 3,
-    "Absorption" = 4,
-    "Float" = 5,
-    "Storage" = 6,
-    "Equalize (manual)" = 7,
-    "Inverting" = 9,
-    "Power supply" = 11,
-    "Starting-up" = 245,
-    "Repeated absorption" = 246,
-    "Auto equalize / Recondition" = 247,
-    "BatterySafe" = 247,
-    "External Control" = 252
-}
-
-enum ErrorMessage {
-    "" = 0,
-    "Battery voltage too high" = 2,
-    "Charger temperature too high" = 17,
-    "Charger over current" = 18,
-    "Charger current reversed" = 19,
-    "Bulk time limit exceeded" = 20,
-    "Current sensor issue (sensor bias/sensor broken)" = 21,
-    "Terminals overheated" = 26,
-    "Converter issue (dual converter models only)" = 28,
-    "Input voltage too high (solar panel)" = 33,
-    "Input current too high (solar panel)" = 34,
-    "Input shutdown (due to excessive battery voltage)" = 38,
-    "Input shutdown (due to current flow during off mode)" = 39,
-    "Lost communication with one of devices" = 65,
-    "Synchronised charging device configuration issue" = 66,
-    "BMS connection lost" = 67,
-    "Network misconfigured" = 68,
-    "Factory calibration data lost" = 116,
-    "Invalid/incompatible firmware" = 117,
-    "User settings invalid" = 119
-
-}
-
-
-enum MPPTMessage {
-    "Off" = 0,
-    "Voltage or current limited" = 1,
-    "MPP Tracker active" = 2,
-}
-
-enum OffReasonMessage {
-    "" = 0,
-    "No input power" = 1,
-    "Switched off (power switch)" = 2,
-    "Switched off (device mode register) " = 4,
-    "Remote input" = 8,
-    "Protection active" = 10,
-    "Paygo" = 20,
-    "BMS" = 40,
-    "Engine shutdown detection" = 80,
-    "Analysing input voltage" = 100,
-}
-
-export enum DeviceType {
-    "MPPT" = 0,
-    "Inverter" = 1,
-    "BMV" = 2,
-    "Charger" = 3
-}
-
-export class VEDirectPnP_UnsupportedDeviceData {
+interface IVEDirectPnP_DeviceData {
     deviceName: string;
     deviceSN: string;
-    VEDirectData: Object;
-    constructor(VEDirectData) {
+    VEDirectData: VEDirectData;
+}
+
+export class VEDirectPnP_UnsupportedDeviceData implements IVEDirectPnP_DeviceData {
+    deviceName: string;
+    deviceSN: string;
+    VEDirectData: VEDirectData;
+    constructor(VEDirectRawData) {
         //VE.Direct -> UnsupportedDeviceData properties mapping
-        this.deviceName = getDeviceName(VEDirectData["PID"]);
-        this.deviceSN = VEDirectData["SER#"];
-        this.VEDirectData = VEDirectData;
+        const data = new VEDirectData(VEDirectRawData);
+        this.deviceName = getDeviceName(data["PID"]);
+        this.deviceSN = data["SER#"];
+        this.VEDirectData = data;
     }
 }
 
-export class VEDirectPnP_MPPTDeviceData {
+export class VEDirectPnP_MPPTDeviceData implements IVEDirectPnP_DeviceData {
     deviceType: string;
     deviceName: string;
     deviceSN: string;
@@ -104,32 +44,33 @@ export class VEDirectPnP_MPPTDeviceData {
     relayState: boolean;
     offReasonMessage: string;
     daySequenceNumber: number;
-    VEDirectData: Object;
-    constructor(VEDirectData) {
+    VEDirectData: VEDirectData;
+    constructor(VEDirectRawData) {
         //VE.Direct -> MPPTDeviceData properties mapping
-        this.deviceName = getDeviceName(VEDirectData["PID"]);
-        this.deviceSN = VEDirectData["SER#"];
+        const data = new VEDirectData(VEDirectRawData);
+        this.deviceName = getDeviceName(data["PID"]);
+        this.deviceSN = data["SER#"];
         this.deviceType = DeviceType[0];
-        this.deviceFirmwareVersion = getDeviceFW(VEDirectData);
-        this.batteryVoltage = VEDirectData["V"] / 1000; //mV -> V
-        this.batteryCurrent = VEDirectData["I"] / 1000; //mA -> A
-        this.statusMessage = StatusMessage[VEDirectData["CS"]];
-        this.errorMessage = ErrorMessage[VEDirectData["ERR"]];
-        this.mpptMessage = MPPTMessage[VEDirectData["MPPT"]];
-        this.maximumPowerToday = VEDirectData["H21"]; // W
-        this.maximumPowerYesterday = VEDirectData["H23"]; // W
-        this.totalEnergyProduced = VEDirectData["H19"] / 100; // kWh
-        this.energyProducedToday = VEDirectData["H20"] / 100; // kWh
-        this.energyProducedYesterday = VEDirectData["H22"] / 100; // kWh
-        this.photovoltaicPower = VEDirectData["PPV"]; // W
-        this.photovoltaicVoltage = VEDirectData["VPV"] / 1000; //mV -> V
+        this.deviceFirmwareVersion = getDeviceFW(data);
+        this.batteryVoltage = data["V"] / 1000; //mV -> V
+        this.batteryCurrent = data["I"] / 1000; //mA -> A
+        this.statusMessage = StatusMessage[data["CS"]];
+        this.errorMessage = ErrorMessage[data["ERR"]];
+        this.mpptMessage = MPPTMessage[data["MPPT"]];
+        this.maximumPowerToday = data["H21"]; // W
+        this.maximumPowerYesterday = data["H23"]; // W
+        this.totalEnergyProduced = data["H19"] / 100; // kWh
+        this.energyProducedToday = data["H20"] / 100; // kWh
+        this.energyProducedYesterday = data["H22"] / 100; // kWh
+        this.photovoltaicPower = data["PPV"]; // W
+        this.photovoltaicVoltage = data["VPV"] / 1000; //mV -> V
         this.photovoltaicCurrent = this.photovoltaicPower / this.photovoltaicVoltage; //A
-        this.loadCurrent = VEDirectData["IL"] ? VEDirectData["IL"] / 1000 : 0; //mA -> A
-        this.loadOutputState = getStringBoolean(VEDirectData["LOAD"]);
-        this.relayState = getStringBoolean(VEDirectData["Relay"]);
-        this.offReasonMessage = OffReasonMessage[VEDirectData["OR"]];
-        this.daySequenceNumber = VEDirectData["HSDS"];
-        this.VEDirectData = VEDirectData;
+        this.loadCurrent = data["IL"] ? data["IL"] / 1000 : 0; //mA -> A
+        this.loadOutputState = getStringBoolean(data["LOAD"]);
+        this.relayState = getStringBoolean(data["Relay"]);
+        this.offReasonMessage = OffReasonMessage[data["OR"]];
+        this.daySequenceNumber = data["HSDS"];
+        this.VEDirectData = data;
     }
 }
 
